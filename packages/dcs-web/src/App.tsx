bd9e2bf19f1208574829d2ad24caf68556e80b2c
jack <jack.dang@itutorworld.com.au>
2023-12-13 23:19:14 +1100

    cleaning up

--- packages/dcs-web/src/App.tsx
+++ packages/dcs-web/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import MainLayout from './routes/MainLayout';
 import { DayNightThemeProvider } from './components/DayNight/DayNightContext';
-import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RouteItemConfig, routeConfigs } from './routes/routeConfig';
 import _ from 'lodash';
 
